7d96760f0c2839607fd9acf2e375503b3cb77162
matheusauthor <matheus.author@gmail.com>
2018-03-16 17:27:05 -0300

    1

---
+++ backups/matheuszeuch.sql
@@ -0,0 +1,14 @@
+CREATE TABLE sqlite_sequence(name,seq);
+INSERT INTO sqlite_sequence(name,seq) VALUES ("Event","4");
+CREATE TABLE Account ( AccountID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, AccountTypeID INTEGER);
+CREATE TABLE AccountType ( AccountTypeID INTEGER PRIMARY KEY, Name TEXT);
+CREATE TABLE Category ( CategoryID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, ParentCategoryID INTEGER, CategoryTypeID INTEGER);
+CREATE TABLE CategoryType ( CategoryTypeID INTEGER PRIMARY KEY, Name TEXT, Multiplier INTEGER DEFAULT -1);
+INSERT INTO CategoryType(CategoryTypeID,Name,Multiplier) VALUES ("1","Receitas","1");
+INSERT INTO CategoryType(CategoryTypeID,Name,Multiplier) VALUES ("2","Despesas","-1");
+CREATE TABLE Recurrence ( RecurrenceID INTEGER PRIMARY KEY AUTOINCREMENT, CategoryID INTEGER, Amount REAL, Description TEXT, RecurrenceTypeID INTEGER, RecurrenceValue INTEGER);
+CREATE TABLE Event ( EventID INTEGER PRIMARY KEY AUTOINCREMENT, Date INTEGER, Amount REAL, CategoryID INTEGER, Description TEXT, AccountID INTEGER);
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("1","null","5,00","null","Café","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("2","null","26,00","null","Almoço","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("3","null","16,00","null","Janta","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("4","null","30,50","null","Presente","null");
---
+++ backups/websqldump.sql
@@ -0,0 +1,18 @@
+CREATE TABLE sqlite_sequence(name,seq);
+INSERT INTO sqlite_sequence(name,seq) VALUES ("Event","8");
+CREATE TABLE Account ( AccountID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, AccountTypeID INTEGER);
+CREATE TABLE AccountType ( AccountTypeID INTEGER PRIMARY KEY, Name TEXT);
+CREATE TABLE Category ( CategoryID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, ParentCategoryID INTEGER, CategoryTypeID INTEGER);
+CREATE TABLE CategoryType ( CategoryTypeID INTEGER PRIMARY KEY, Name TEXT, Multiplier INTEGER DEFAULT -1);
+INSERT INTO CategoryType(CategoryTypeID,Name,Multiplier) VALUES ("1","Receitas","1");
+INSERT INTO CategoryType(CategoryTypeID,Name,Multiplier) VALUES ("2","Despesas","-1");
+CREATE TABLE Recurrence ( RecurrenceID INTEGER PRIMARY KEY AUTOINCREMENT, CategoryID INTEGER, Amount REAL, Description TEXT, RecurrenceTypeID INTEGER, RecurrenceValue INTEGER);
+CREATE TABLE Event ( EventID INTEGER PRIMARY KEY AUTOINCREMENT, Date INTEGER, Amount REAL, CategoryID INTEGER, Description TEXT, AccountID INTEGER);
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("1","null","5,00","null","Café","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("2","null","20,00","null","Almoço","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("3","null","15,00","null","Janta","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("4","null","180,00","null","Combustível","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("5","null","25,00","null","Presente","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("6","null","3,00","null","Sorvete","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("7","null","3,50","null","Café com Lemos","null");
+INSERT INTO Event(EventID,Date,Amount,CategoryID,Description,AccountID) VALUES ("8","null","25","null","Teste","null");
